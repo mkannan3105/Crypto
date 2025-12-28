a8f7ae9d5e3ef02cdb09fc493b90987d8afcd1be
M Kannan <mkannan3105@gmail.com>
2025-12-28 19:42:14 +0530

    Initial commit - Crypto project

---
+++ config/config.py
@@ -0,0 +1,17 @@
+import os
+
+# Paths
+EXTENSION_PATH = r"C:\Users\mkann\Downloads\metamask-chrome-12.20.1"
+USER_DATA_DIR = os.path.join(os.getcwd(), "playwright_profile")
+
+# MetaMask Credentials
+SEED_WORDS = [
+    "test", "test", "test", "test", "test", "test",
+    "test", "test", "test", "test", "test", "test"
+]
+
+METAMASK_PASSWORD = "password"
+
+# URLs
+METAMASK_URL = "chrome-extension://gjikmpkaongdckelejoafeogbokmnddg/home.html#onboarding/welcome"
+ETHEREARL_URL = "https://testnet.ethereal.trade/trade/BTCUSD"
