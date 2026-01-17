3956991d40587124da281e6efe6145b4fc5398ce
M Kannan <mkannan3105@gmail.com>
2026-01-17 17:08:14 +0530

    Recent changes.

--- pages/project_page.py
+++ pages/project_page.py
@@ -10,34 +10,37 @@ class ProjectPage:
 
     def wallet_connect_pop(self):
         # Handle MetaMask connection popup
-        time.sleep(1)
+        time.sleep(2)
         popup = self.page.context.wait_for_event("page")
         popup.wait_for_load_state()
-        popup.wait_for_selector("//*[@data-testid='confirm-btn']", timeout=30000)
+        popup.wait_for_selector("//*[@data-testid='confirm-btn']", timeout=50000)
         popup.get_by_test_id("confirm-btn").click()
         popup.close()
 
     def wallet_confirmation_pop(self):
+         time.sleep(2)
          # Confirm MetaMask transaction
          popup = self.page.context.wait_for_event("page")
          popup.wait_for_load_state()
-         popup.wait_for_selector("[data-testid='confirm-footer-button']", timeout=30000)
+         popup.wait_for_selector("[data-testid='confirm-footer-button']", timeout=50000)
          popup.locator("[data-testid='confirm-footer-button']").click()
          #popup.locator("//*[text()='Confirm']").click()
          popup.close()
 
     def wallet_confirmation_next_pop(self):
          # Confirm MetaMask transaction
+         time.sleep(2)
          popup = self.page.context.wait_for_event("page")
          popup.wait_for_load_state()
-         popup.wait_for_selector("[data-testid='page-container-footer-next']", timeout=30000)
+         popup.wait_for_selector("[data-testid='page-container-footer-next']", timeout=50000)
          popup.locator("[data-testid='page-container-footer-next']").click()
          popup.close()
 
     def wallet_approve_pop(self):
          # Handle transaction confirmation popup
+         time.sleep(2)
          popup = self.page.context.wait_for_event("page")
-         popup.wait_for_selector("[data-testid='confirmation-submit-button']", timeout=30000)
+         popup.wait_for_selector("[data-testid='confirmation-submit-button']", timeout=100000)
          popup.locator("[data-testid='confirmation-submit-button']").click()
          #popup.close()
 
@@ -115,19 +118,15 @@ class ProjectPage:
         # Handle transaction approve popup
         self.wallet_approve_pop()
         time.sleep(2)
-        self.page.get_by_role("link", name="Launch App").click()
+        self.page.locator("// *[text() = 'Swap']").click()
+        time.sleep(2)
+        #self.page.get_by_role("link", name="Launch App").click()
         self.page.get_by_role("button").filter(has_text=re.compile(r"^$")).nth(1).click()
         # Swap Amount
         amount_input = self.page.get_by_placeholder("0.00").first
         expect(amount_input).to_be_visible(timeout=30000)
-        amount_input.fill("0.005")
+        amount_input.fill("0.01")
         time.sleep(2)
-        # Swap button
-        #swap = self.page.locator(ethereal.ARC_SWAP_BTN)
-        #swap.wait_for(state="attached", timeout=30000)
-        #time.sleep(2)
-        #self.page.locator(ethereal.ARC_SWAP_BTN).click()
-        #time.sleep(3)
         self.page.evaluate("window.scrollBy(0, 500)")
         time.sleep(2)
         approve_eurc = self.page.get_by_role("button", name="Approve EURC")
@@ -139,17 +138,7 @@ class ProjectPage:
         else:
             self.page.get_by_role("button", name="Swap").click()
             self.wallet_confirmation_pop()
-        #self.page.evaluate("() => debugger")
-        #self.page.pause()
-        #approve_eurc = self.page.locator(ethereal.ARC_APPROVE_EURC_BTN)
-        #approve_usdc = self.page.locator(ethereal.ARC_APPROVE_USDC_BTN)
-        #if approve_eurc.is_visible():
-        #    approve_eurc.click()
-        #else:
-        #    print("Commin else")
-        #    self.page.locator(ethereal.ARC_SWAP_BTN).click()
-        #time.sleep(2)
-        #self.wallet_confirmation_pop()
+        print("Swap successful")
         # Add Pools
         self.page.get_by_role("link", name="Pools").click()
         self.page.get_by_role("button", name="Add").click()
@@ -188,17 +177,16 @@ class ProjectPage:
         time.sleep(2)
         if button.is_visible():
             self.page.locator("//*[@class='text-gray-400']").click()
+        print("Pools Added Successful")
         # Bridge
         self.page.get_by_role("link", name="Bridge").click()
         amount = self.page.get_by_placeholder("0.00")
         amount.click()
         amount.fill("0.0001")
         time.sleep(2)
-
         bridge = self.page.get_by_role(
             "button", name="Bridge to Ethereum Sepolia"
         )
-
         # Wait until button is actually ready
         bridge.wait_for(state="visible", timeout=30000)
         self.page.evaluate("window.scrollBy(0, 500)")
@@ -214,22 +202,136 @@ class ProjectPage:
         self.wallet_confirmation_next_pop()
         # Handle transaction approve popup
         self.wallet_confirmation_pop()
-        """
+        print("Bridge Successful")
+        # Vault
         self.page.get_by_role("link", name="Vault").click()
         self.page.get_by_role("button", name="Deposit USDC").click()
         self.page.get_by_placeholder("0.00").click()
-        self.page.get_by_placeholder("0.00").fill("0.01")
-        #self.page.pause()
+        self.page.get_by_placeholder("0.00").fill("0.001")
         approve_usdc = self.page.get_by_role("button", name="Approve USDC")
         if approve_usdc.is_visible():
             approve_usdc.click()
+            print("Add Amount")
             # Handle transaction approve popup
             self.wallet_confirmation_pop()
+            time.sleep(5)
             self.page.get_by_role("button", name="Deposit", exact=True).click()
             # Handle transaction approve popup
             self.wallet_confirmation_pop()
         else:
+            print("Add Amount")
             self.page.get_by_role("button", name="Deposit", exact=True).click()
             # Handle transaction approve popup
             self.wallet_confirmation_pop()
-        """
+        print("Vault Successful")
+
+    def testnet_hotstuff_trade(self):
+        time.sleep(1)
+        self.page.get_by_role("button", name="Connect Wallet").click()
+        self.page.get_by_role("button", name="MetaMask-icon MetaMask").click()
+        # Handle MetaMask connection popup
+        self.wallet_connect_pop()
+        time.sleep(2)
+        already_ref = self.page.locator("// *[text() = 'You are already using a referral code and cannot change it.']")
+        if already_ref.is_visible():
+            self.page.locator("//*[text()='Continue to Trading']").click()
+            time.sleep(1)
+        join_Code = self.page.get_by_role("button", name="Join with Code")
+        if join_Code.is_visible():
+           self.page.get_by_role("button", name="Join with Code").click()
+           time.sleep(1)
+        self.page.get_by_role("button", name="Faucet").first.click()
+        self.page.get_by_role("button", name="Claim Testnet Tokens").click()
+        self.page.get_by_role("button", name="Faucet").first.click()
+        self.page.get_by_role("combobox").click()
+        self.page.get_by_role("option", name="USDT USDT").click()
+        self.page.get_by_role("button", name="Claim Testnet Tokens").click()
+        self.page.get_by_role("tab", name="Market").click()
+        self.page.get_by_role("textbox").first.click()
+        self.page.get_by_role("textbox").first.fill("10")
+        self.page.get_by_role("button", name="Enable Trading").click()
+        time.sleep(2)
+        self.page.get_by_role("button", name="Confirm").click()
+        # Handle transaction approve popup
+        self.wallet_confirmation_pop()
+        #self.page.pause()
+        if self.page.get_by_role("button", name="Claim Testnet Tokens").is_visible():
+            self.page.get_by_role("button", name="Claim Testnet Tokens").click()
+            time.sleep(1)
+        #self.page.pause()
+        self.page.get_by_role("button", name="Buy/Long").click()
+        self.page.get_by_role("button", name="Buy / Long").click()
+        self.page.get_by_role("button", name="Sell/Short").click()
+        self.page.get_by_role("button", name="Sell / Short").click()
+        self.page.get_by_role("button", name="BTC-PERP BTC-PERP 50x").click()
+        self.page.get_by_label("BTC-PERP50x").get_by_text("ETH-PERP25x").click()
+        self.page.get_by_role("textbox").first.click()
+        self.page.get_by_role("textbox").first.fill("10")
+        self.page.get_by_role("button", name="Buy/Long").click()
+        self.page.get_by_role("button", name="Buy / Long").click()
+        self.page.get_by_role("button", name="Sell/Short").click()
+        self.page.get_by_role("button", name="Sell / Short").click()
+        self.page.get_by_role("button", name="ETH-PERP ETH-PERP 25x").click()
+        self.page.get_by_label("ETH-PERP25x").get_by_text("HYPE-PERP").click()
+        self.page.get_by_role("textbox").first.click()
+        self.page.get_by_role("textbox").first.fill("10")
+        self.page.get_by_role("button", name="Buy/Long").click()
+        self.page.get_by_role("button", name="Buy / Long").click()
+        self.page.get_by_role("button", name="Sell/Short").click()
+        self.page.get_by_role("button", name="Sell / Short").click()
+        print("Swap Successful")
+        self.page.get_by_role("button", name="Vaults").click()
+        self.page.locator("div").filter(has_text=re.compile(r"^Deposit$")).nth(1).click()
+        self.page.get_by_role("spinbutton").click()
+        self.page.get_by_role("spinbutton").fill("10")
+        self.page.get_by_role("button", name="Deposit").click()
+        print("Deposit Successful")
+        #self.page.pause()
+
+    def testnet_x1ecochain(self):
+        self.page.get_by_role("button", name="Connect").click()
+        self.page.get_by_test_id("rk-wallet-option-metaMask").click()
+        # Handle MetaMask connection popup
+        self.wallet_connect_pop()
+        # Handle transaction approve popup
+        self.wallet_confirmation_pop()
+        # Handle transaction approve popup
+        self.wallet_approve_pop()
+        #self.page.pause()
+        #self.page.get_by_role("button", name="Claim X1T").click()
+        #self.page.get_by_role("button", name="Request").click()
+        #time.sleep(2)
+        #if self.page.locator(".Cross > path").is_visible():
+        #    self.page.locator(".Cross > path").click(timeout=20000)
+        #self.page.pause()
+        self.page.get_by_role("button", name="Get ECO Points").click()
+        time.sleep(2)
+        self.page.evaluate("window.scrollBy(0, 800)")
+        time.sleep(1)
+        #self.page.pause()
+        #self.page.get_by_role("button", name="Claim", exact=True).click()
+        #self.page.get_by_role("button", name="Claim").click()
+        self.page.locator("//button[text()='Claim']").click()
+        self.page.locator(
+            "div:nth-child(4) > .QuestsContent > .Quests > div:nth-child(2) > .Quest > .Content > .BottomContent > .ButtonElement").click()
+        time.sleep(2)
+        self.page.get_by_role("button", name="Request").click()
+        time.sleep(2)
+        self.page.locator(".Cross > path").click()
+        time.sleep(2)
+        self.page.locator("//button[text()='Claim']").click()
+        time.sleep(2)
+        self.page.locator(
+            "div:nth-child(4) > .QuestsContent > .Quests > div > .Quest > .Content > .BottomContent > .ButtonElement").click()
+        time.sleep(2)
+        self.page.get_by_role("button", name="0,05 X1T").click()
+        time.sleep(2)
+        self.page.get_by_role("button", name="Random Address").click()
+        time.sleep(2)
+        self.page.get_by_role("button", name="Send X1T Coins").click()
+        time.sleep(2)
+        # Handle transaction approve popup
+        self.wallet_confirmation_pop()
+        self.page.locator(".Cross").click()
+        time.sleep(2)
+        self.page.locator("//button[text()='Claim']").click()
